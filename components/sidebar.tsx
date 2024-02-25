17bbfa67ce552e5fbe283fdffc015a97ad38a5ba
David-C456 <80932927+David-C456@users.noreply.github.com>
2024-02-25 11:38:08 -0500

    Second Commit

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,6 +4,8 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
+import Box from  "./Box";
+import SidebarItem from "./SidebarItem";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -27,13 +29,42 @@ const Sidebar: React.FC<SidebarProps> = ({
             active: pathname === '/search',
             href: '/search',
         }
-    ], []);
+    ], [pathname]);
 
     return (
         <div className="flex h-full">
-          className="
-          hidden
-          "
+          <div 
+            className="
+                hidden
+                md:flex
+                flex-col
+                gap-y-2
+              bg-black
+                h-full
+                w-[300px]
+                p-2
+            "
+          >
+            <Box>
+                <div className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                ">
+                    {routes.map((item =>
+                        <SidebarItem
+                            key={item.label}
+                            {...item}
+                        />
+                    ))}
+                </div>
+            </Box>
+            <Box className="overflow-y-auto h-full">
+                Song Library
+            </Box>
+          </div>
         </div>
     );
 }
